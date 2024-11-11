67e1b65a4e86415886f69863bfd28597ebd43233
hareesh <hareesh.sada@gmail.com>
2024-11-11 22:40:33 +0530

    my first commit

---
+++ TextSummarizationBOT/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(
+    model="mixtral-8x7b-32768",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+ai_msg = llm.invoke(messages)
+
+print(ai_msg.content)
+ 
